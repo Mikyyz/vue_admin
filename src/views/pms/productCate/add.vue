9b099c81edd050753c8bffdcfb3cb45c9f3c8823
Mikyyz <youzhi.ye1992@gmail.com>
2021-04-13 01:12:02 +0800

    增加产品组件

---
+++ src/views/pms/productCate/add.vue
@@ -0,0 +1,17 @@
+<template>
+    <product-cate-detail />
+</template>
+
+<script>
+import ProductCateDetail from './components/ProductCateDetail'
+    export default {
+        name: 'addProductCate',
+        components: {
+            ProductCateDetail
+        }
+    }
+</script>
+
+<style lang="less" scoped>
+
+</style>
