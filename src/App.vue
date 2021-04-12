9964e7603f2af3a8228c1849127807b38ed66855
Mikyyz <youzhi.ye1992@gmail.com>
2021-04-12 22:55:03 +0800

    全局配置中文

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,32 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <a-config-provider :locale="zh_CN">
+      <div id="app">
+        <router-view :key="$route.path" />
+      </div>
+    </a-config-provider>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import zh_CN from 'ant-design-vue/lib/locale-provider/zh_CN';
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+
+  },
+  data() {
+    return {
+      url: '',
+      zh_CN
+    }
+  },
+  mounted () {
+    
+  },
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  height: 100%;
 }
 </style>
