efc1246d068d466382e7f734c871a33032cbbfc8
Mikyyz <youzhi.ye1992@gmail.com>
2021-04-13 00:45:26 +0800

    图表相关的组件

---
+++ src/components/index.js
@@ -0,0 +1,21 @@
+import ChartCard from '@/components/Charts/ChartCard'
+import MiniArea from '@/components/Charts/MiniArea'
+import MiniBar from '@/components/Charts/MiniBar'
+import MiniProgress from '@/components/Charts/MiniProgress'
+import RankList from '@/components/Charts/RankList'
+import Bar from '@/components/Charts/Bar'
+import Trend from '@/components/Charts/Trend'
+import NumberInfo from '@/components/Charts/NumberInfo'
+import MiniSmoothArea from '@/components/Charts/MiniSmoothArea'
+
+export {
+    ChartCard,
+    MiniArea,
+    MiniBar,
+    MiniProgress,
+    RankList,
+    Bar,
+    Trend,
+    NumberInfo,
+    MiniSmoothArea
+}
