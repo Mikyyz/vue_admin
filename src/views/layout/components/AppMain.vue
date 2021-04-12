68b5f5de7dbaff3184e0959975da19611ab16d22
Mikyyz <youzhi.ye1992@gmail.com>
2021-04-13 01:00:29 +0800

    AppMain组件

---
+++ src/views/layout/components/AppMain.vue
@@ -0,0 +1,66 @@
+<template>
+    <a-layout-content>
+        <router-view />
+    </a-layout-content>
+</template>
+
+<script>
+    export default {
+        name: 'AppMain'
+    }
+</script>
+
+<style lang="less">
+/* 修改 ant-tabs 样式 */
+  .tab-layout-tabs.ant-tabs {
+    border-bottom: 1px solid #ccc;
+    border-left: 1px solid #ccc;
+    background-color: white;
+    padding: 0 20px;
+
+    .ant-tabs-bar {
+      margin: 4px 0 0;
+      border: none;
+    }
+
+  }
+
+  .tab-layout-tabs.ant-tabs {
+
+    &.ant-tabs-card .ant-tabs-tab {
+
+      padding: 0 24px !important;
+      background-color: white !important;
+      margin-right: 10px !important;
+
+      .ant-tabs-close-x {
+        width: 12px !important;
+        height: 12px !important;
+        opacity: 0 !important;
+        cursor: pointer !important;
+        font-size: 12px !important;
+        margin: 0 !important;
+        position: absolute;
+        top: 36%;
+        right: 6px;
+      }
+
+      &:hover .ant-tabs-close-x {
+        opacity: 1 !important;
+      }
+
+    }
+
+  }
+
+  .tab-layout-tabs.ant-tabs.ant-tabs-card > .ant-tabs-bar {
+    .ant-tabs-tab {
+      border: none !important;
+      border-bottom: 1px solid transparent !important;
+    }
+    .ant-tabs-tab-active {
+      border-color: @primary-color!important;
+    }
+  }
+
+</style>
