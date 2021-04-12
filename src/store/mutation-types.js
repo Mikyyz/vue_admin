44ac55992cd562405bbed9bb6a951dbb029808b3
Mikyyz <youzhi.ye1992@gmail.com>
2021-04-13 00:56:03 +0800

    定义mutation-type常量

---
+++ src/store/mutation-types.js
@@ -0,0 +1,16 @@
+// 定义mutation-type常量
+/**
+ * user相关
+ */
+export const SET_TOKEN = 'SET_TOKEN'
+export const SET_USERNAME = 'SET_USERNAME'
+export const SET_AVATAR = 'SET_AVATAR'
+export const SET_ROLES = 'SET_ROLES'
+export const TOGGLE_THEME = 'TOGGLE_THEME'
+export const DEFAULT_COLOR = 'DEFAULT_COLOR'
+
+
+/**
+ * 权限相关
+ */
+export const SET_MENUS = 'SET_MENUS'
