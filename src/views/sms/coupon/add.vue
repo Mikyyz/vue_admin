8c7aa6e1755fbeb93a98473bfc9a1523e36525a5
Mikyyz <youzhi.ye1992@gmail.com>
2021-04-13 01:14:53 +0800

    增加优惠券组件

---
+++ src/views/sms/coupon/add.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <router-view name="addCoupon">增加</router-view>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="less" scoped>
+
+</style>
