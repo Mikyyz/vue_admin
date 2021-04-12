96941444fc5e188b6296faf9e9df6b12dff1a0b9
Mikyyz <youzhi.ye1992@gmail.com>
2021-04-13 01:12:36 +0800

    新增更新产品组件

---
+++ src/views/pms/productCate/update.vue
@@ -0,0 +1,16 @@
+<template>
+    <product-cate-detail />
+</template>
+
+<script>
+import ProductCateDetail from './components/ProductCateDetail';
+    export default {
+        name: 'updateProductCate',
+        components: { ProductCateDetail }
+    }
+</script>
+
+<style lang="
+                ProductAttrDetailless" scoped>
+
+</style>
