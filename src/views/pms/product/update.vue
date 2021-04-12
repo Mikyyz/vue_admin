716dc84d2e371a3db92f6e46fc1bddcb12055958
Mikyyz <youzhi.ye1992@gmail.com>
2021-04-13 01:08:46 +0800

    更新产品组件

---
+++ src/views/pms/product/update.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="update-product">
+        <product-detail />
+    </div>
+</template>
+
+<script>
+import ProductDetail from './components/ProductDetail'
+    export default {
+        name: 'updateProduct',
+        components: {
+            ProductDetail
+        }
+    }
+</script>
+
+<style lang="less" scoped>
+
+</style>
