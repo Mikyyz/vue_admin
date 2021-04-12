231cde7ad6742a32a141675d7e02a00e0ac12a3b
Mikyyz <youzhi.ye1992@gmail.com>
2021-04-13 00:59:33 +0800

    增加404组件

---
+++ src/views/errorPage/404.vue
@@ -0,0 +1,23 @@
+<template>
+    <a-result status="404" title="404" sub-title="抱歉,该页面不存在">
+    <template #extra>
+      <a-button type="primary" @click="backHome">
+        返回首页
+      </a-button>
+    </template>
+  </a-result>
+</template>
+
+<script>
+    export default {
+        methods: {
+            backHome() {
+                this.$router.push({path: '/home'})
+            }
+        },
+    }
+</script>
+
+<style lang="less" scoped>
+
+</style>
